59157d0b3987c70e96675b7b557128e0b7f66f77
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2015-03-30 20:08:31 -0700

    basic upload
    Dude like is this even how you upload VB stuff onto github lol

---
+++ 7SEGdisplay/7SEGdisplay/segmentMachine.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class segmentMachine
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(segmentMachine))
+        Me.graphicSevenSeg = New System.Windows.Forms.PictureBox()
+        Me.toggleActiveLow = New System.Windows.Forms.CheckBox()
+        Me.toggleHex = New System.Windows.Forms.CheckBox()
+        CType(Me.graphicSevenSeg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'graphicSevenSeg
+        '
+        Me.graphicSevenSeg.Image = CType(resources.GetObject("graphicSevenSeg.Image"), System.Drawing.Image)
+        Me.graphicSevenSeg.Location = New System.Drawing.Point(12, 12)
+        Me.graphicSevenSeg.Name = "graphicSevenSeg"
+        Me.graphicSevenSeg.Size = New System.Drawing.Size(89, 119)
+        Me.graphicSevenSeg.TabIndex = 0
+        Me.graphicSevenSeg.TabStop = False
+        '
+        'toggleActiveLow
+        '
+        Me.toggleActiveLow.AutoSize = True
+        Me.toggleActiveLow.Location = New System.Drawing.Point(12, 152)
+        Me.toggleActiveLow.Name = "toggleActiveLow"
+        Me.toggleActiveLow.Size = New System.Drawing.Size(101, 17)
+        Me.toggleActiveLow.TabIndex = 1
+        Me.toggleActiveLow.Text = "Use Active Low"
+        Me.toggleActiveLow.UseVisualStyleBackColor = True
+        '
+        'toggleHex
+        '
+        Me.toggleHex.AutoSize = True
+        Me.toggleHex.Cursor = System.Windows.Forms.Cursors.Default
+        Me.toggleHex.Location = New System.Drawing.Point(12, 175)
+        Me.toggleHex.Name = "toggleHex"
+        Me.toggleHex.Size = New System.Drawing.Size(96, 17)
+        Me.toggleHex.TabIndex = 2
+        Me.toggleHex.Text = "Display in HEX"
+        Me.toggleHex.UseVisualStyleBackColor = True
+        '
+        'segmentMachine
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 261)
+        Me.Controls.Add(Me.toggleHex)
+        Me.Controls.Add(Me.toggleActiveLow)
+        Me.Controls.Add(Me.graphicSevenSeg)
+        Me.HelpButton = True
+        Me.Name = "segmentMachine"
+        Me.Text = "The Magical Segment Machine"
+        CType(Me.graphicSevenSeg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents graphicSevenSeg As System.Windows.Forms.PictureBox
+    Friend WithEvents toggleActiveLow As System.Windows.Forms.CheckBox
+    Friend WithEvents toggleHex As System.Windows.Forms.CheckBox
+
+End Class
---
+++ 7SEGdisplay/7SEGdisplay/segmentMachine.vb
@@ -0,0 +1,50 @@
+﻿Public Class segmentMachine
+
+
+
+
+    ' Checkbox actions
+
+    Private Sub toggleActiveLow_CheckedChanged(sender As Object, e As System.EventArgs) Handles toggleActiveLow.CheckedChanged
+        MsgBox("I'm annoying!")
+    End Sub
+
+
+    Private Sub toggleHex_CheckedChanged(sender As Object, e As System.EventArgs) Handles toggleHex.CheckedChanged
+        MsgBox("You did it!")
+    End Sub
+
+    ' All the dirty seven-segment work
+
+    Private Sub graphicSevenSeg_Click(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles graphicSevenSeg.Click
+        ' Check if segA
+        If ((e.X > 27 And e.X < 80) And (e.Y > 10 And e.Y < 20)) Then
+            MsgBox("Segment A hit!")
+            'Check if segB
+        ElseIf ((e.X > 69 And e.X < 82) And (e.Y > 18 And e.Y < 58)) Then
+            MsgBox("Segment B hit!")
+            'Check if segC
+        ElseIf ((e.X > 61 And e.X < 73) And (e.Y > 64 And e.Y < 108)) Then
+            MsgBox("Segment C hit!")
+            'Check if segD
+        ElseIf ((e.X > 11 And e.X < 63) And (e.Y > 100 And e.Y < 111)) Then
+            MsgBox("Segment D hit!")
+            'Check if segE
+        ElseIf ((e.X > 8 And e.X < 22) And (e.Y > 64 And e.Y < 86)) Then
+            MsgBox("Segment E hit!")
+            'Check if segF
+        ElseIf ((e.X > 16 And e.X < 30) And (e.Y > 13 And e.Y < 60)) Then
+            MsgBox("Segment F hit!")
+            'Check if segG
+        ElseIf ((e.X > 19 And e.X < 71) And (e.Y > 55 And e.Y < 66)) Then
+            MsgBox("Segment G hit!")
+            'Check if DP
+        ElseIf ((e.X > 72 And e.X < 84) And (e.Y > 104 And e.Y < 116)) Then
+            MsgBox("DP hit!")
+
+        End If
+
+    End Sub
+
+
+End Class
